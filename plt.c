950a7a840e8475e21c2b80433f33692b91bf9e44
zys1310992814 <1310992814@qq.com>
2024-02-03 02:38:41 +0800

    update-2024-2-3

--- plt.c
+++ plt.c
@@ -3,7 +3,8 @@
 
 int main(int argc, char **argv)
 {
-	int *s=0x804c00c;  //桩的地址
+	int *s=0x804c010;  //桩的地址
 	printf("The address of  first call printf function is %p\n", *s); //第1次调用时的地址
 	printf("The address of second call printf function is %p\n", *s); //第2次调用时的地址
+	printf("The address of second call printf function is %p\n", *s); //第3次调用时的地址
 }
